1d4591ae36fc9bfd83e9c424d3bb25602ec37156
Jonas Porto <jporto.contato@hotmail.com>
2015-10-19 23:10:13 -0200

    new feature to send email to company when a new comment

---
+++ app/mailers/company_mailer.rb
@@ -0,0 +1,16 @@
+class CompanyMailer < ApplicationMailer
+  
+  default from: "jporto.contato@hotmail.com"
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.company_mailer.new_comment.subject
+  #
+  def new_comment(job, comment)
+  	@job     = job
+  	@comment = comment
+  	@company = job.company
+
+    mail to: @company.email, subject: "New comment received"
+  end
+end
